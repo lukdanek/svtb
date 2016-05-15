a5c89a5dee5b584ff22d8013fe07083e25a9c518
lukdanek <luk.danek@gmail.com>
2016-05-15 15:41:56 +0200

    new content

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ $(document).ready(function() {
 	// 07. COUNTDOWN
 	//===================================================================================
 	var theday = new Date();
-	theday = new Date(2016, 8, 26, 13);
+	theday = new Date(2016, 7, 26, 13);
 	$('#countdown').countdown({until: theday, format: 'WDHMS'});
 	$('#countdown').countdown($.countdown.regionalOptions['custom-label']); 
 		 
